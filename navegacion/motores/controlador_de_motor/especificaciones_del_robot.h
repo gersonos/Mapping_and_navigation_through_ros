7c17c6e8badab394c6c66b7e86f0b28863d2cd12
gersonos <gersononail@hotmail.com>
2017-09-22 18:59:08 -0300

    Add files via upload
    Files used for navigation stack

---
+++ navegacion/motores/controlador_de_motor/especificaciones_del_robot.h
@@ -0,0 +1,12 @@
+#ifndef ESPECIFICACIONES_DEL_ROBOT_H
+#define ESPECIFICACIONES_DEL_ROBOT_H
+
+#define pulso_encoders             16  // conteos por revolución de los encoders en cada canal
+#define relacion_transformacion    131.25  // relación de transformación propia del motorreductor c/encoder
+#define diametro_ruedas            0.14   //diámetro en m
+#define ancho_ruedas               0.037   // ancho o espesor de las ruedas en m
+#define distancia_ruedas           0.4   // distancia de separación de las ruedas (...creo) en m
+#define MAX_RPM                    16    // velocidad máxima de los motorreductores sin carga al parecer
+#define pi                         3.1415926  
+#define dos_pi                     6.2831853
+#endif
